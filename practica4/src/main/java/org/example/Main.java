92d3f090765893865594706592a3243fbda79a52
Frias Luna <starlinluna0102@gmail.com>
2024-02-21 09:46:58 -0400

    inicial.

---
+++ practica4/src/main/java/org/example/Main.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+import io.javalin.rendering.JavalinRenderer;
+import io.javalin.rendering.template.JavalinThymeleaf;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        //Creando la instancia del servidor y configurando.
+        Javalin app = Javalin.create(config -> {
+            //configurando los documentos estaticos.
+            config.staticFiles.add(staticFileConfig -> {
+                staticFileConfig.hostedPath = "/";
+                staticFileConfig.directory = "/publico";
+                staticFileConfig.location = Location.CLASSPATH;
+                staticFileConfig.precompress = false;
+                staticFileConfig.aliasCheck = null;
+                JavalinRenderer.register(new JavalinThymeleaf(), ".html");
+
+            });
+
+        });
+        app.start(getHerokuAssignedPort());
+
+
+        app.get("/",cxt ->{
+            cxt.redirect("index.html");
+        });
+
+    }
+
+
+
+
+
+
+        static int getHerokuAssignedPort() {
+            ProcessBuilder processBuilder = new ProcessBuilder();
+            if (processBuilder.environment().get("PORT") != null) {
+                return Integer.parseInt(processBuilder.environment().get("PORT"));
+            }
+            return 8000; //Retorna el puerto por defecto en caso de no estar en Heroku.
+        }
+
+
+
+
+}
+
+
+
